bb98c45649d212182f5f5e3a6c228156afe754f8
Scott Evans <michealsevans20@gmail.com>
2022-05-03 20:08:56 -0500

    added the routes and about me section

---
+++ src/About.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+// import './About.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div className='about-root'>
+        <div className='about-us-container'>
+          <h2>Welcome to our site!</h2>
+          <hr />
+          <p>We'd like to introduce you to the software developers that gave a lot to bring this application together!
+          </p>
+          <hr />
+          <h3>Abdulahi Mohamud</h3>
+          <ul className='ul-item'>Pitch</ul>
+          <h3>Raul Zarate</h3>
+          <ul className='ul-item'>Pitch</ul>
+          <h3>Scott Evans</h3>
+          <ul className='ul-item'>Pitch</ul>
+          <h3>Darius Owens</h3>
+          <ul className='ul-item'>Pitch</ul>
+        </div>
+      </div>
+    );
+  }
+}
+export default About;
