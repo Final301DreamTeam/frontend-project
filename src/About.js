317178adf496848e992cbb64cb483c643018fccb
Scott Evans <michealsevans20@gmail.com>
2022-05-05 00:38:54 -0500

    added styling

--- src/About.js
+++ src/About.js
@@ -1,27 +1,73 @@
 import React, { Component } from 'react';
-// import './About.css';
+import { Accordion } from 'react-bootstrap';
+import './About.css';
 
 class About extends Component {
   render() {
     return (
-      <div className='about-root'>
-        <div className='about-us-container'>
-          <h2>Welcome to our site!</h2>
-          <hr />
-          <p>We'd like to introduce you to the software developers that gave a lot to bring this application together!
-          </p>
-          <hr />
-          <h3>Abdulahi Mohamud</h3>
-          <ul className='ul-item'>Pitch</ul>
-          <h3>Raul Zarate</h3>
-          <ul className='ul-item'>Pitch</ul>
-          <h3>Scott Evans</h3>
-          <ul className='ul-item'>Pitch</ul>
-          <h3>Darius Owens</h3>
-          <ul className='ul-item'>Pitch</ul>
+      <>
+        <div class="wrap">
+          <br />
+          <h1>Welcome to our site!</h1>
+          <section class="bl">
+            <p>We'd like to introduce you to the software developers that gave a lot to bring this application together!</p>
+          </section>
         </div>
-      </div>
+        <Accordion className="accordion" defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header className="accHeader">Abdulahi Mohamud</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header className="accHeader">Raul Zarate</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header className="accHeader">Scott Evans</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header className="accHeader">Darius Owens</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </>
     );
   }
 }
 export default About;
+
+
+
